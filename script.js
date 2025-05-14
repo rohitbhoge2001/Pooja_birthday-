959d526d6404ad788246eaf83807e358c2ef14fc
rohitbhoge2001 <ROHITBHOGE12@GMAIL.COM>
2025-05-14 18:36:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-// JavaScript logic from the original embedded code goes here
+
+for (let i = 1; i <= 15; i++) {
+  images.push(assets/mem${i}.jpg);
+}
+
+let current = 0;
+
+function startShow() {
+  document.getElementById('start-screen').style.display = 'none';
+  document.getElementById('slideshow').style.display = 'block';
+  document.getElementById('bg-music').play();
+  document.getElementById('slide-img').src = images[current];
+}
+
+function nextImage() {
+  if (current < images.length - 1) {
+    current++;
+    document.getElementById('slide-img').src = images[current];
+  } else {
+    document.getElementById('slideshow').style.display = 'none';
+    document.getElementById('videos').style.display = 'block';
+  }
+}
+
+function prevImage() {
+  if (current > 0) {
+    current--;
+    document.getElementById('slide-img').src = images[current];
+  }
+}
+
+function showFinal() {
+  document.getElementById('videos').style.display = 'none';
+  document.getElementById('final-message').style.display = 'block';
+}
